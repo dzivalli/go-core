1eb106cdcc74ffd0d3b5b0d7e44264e90708ca10
stan <stan@onemob.com>
2020-10-14 19:29:12 -0700

    task 1

---
+++ task-1/cmd/run.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"task-1/pkg/fibonacci"
+)
+
+func main() {
+	fmt.Println(fibonacci.Calculate(5))
+}
---
+++ task-1/go.mod
@@ -0,0 +1,7 @@
+module task-1
+
+go 1.15
+
+replace (
+    task-1/pkg/fibonacci v0.0.0 => ./pkg/fibonacci
+)
