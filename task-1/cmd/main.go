811fa5c269b9edb9267a2617fea4a29e4b45fa25
Stan <stan@onemob.com>
2020-10-16 20:42:18 -0700

    Task 1 extra (#2)
    * task 1
    
    * add arguments
    
    * style
    
    * review suggestions

--- task-1/cmd/main.go
+++ task-1/cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
 	"task-1/pkg/fib"
 )
 
 func main() {
-	fmt.Println(fib.Num(5))
+	var nFlag = flag.Int("n", 1, "fibonacci number")
+	flag.Parse()
+	fmt.Println(fib.Num(*nFlag))
 }
